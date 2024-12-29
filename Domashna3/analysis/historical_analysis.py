38cc4b52ba2102b0601029548c96fdbc9269a6ed
Вашето Име <.email@.com>
2024-12-29 01:12:37 +0100

    adding historical analysis

---
+++ Domashna3/analysis/historical_analysis.py
@@ -0,0 +1,9 @@
+def get_historical_data(conn, stock_name, start_date, end_date):
+    data = conn.execute('''
+        SELECT Датум, "Цена на последна трансакција", "Мак.", "Мин.", "Просечна цена", "%пром.", "Количина", "Промет во БЕСТ во денари", "Вкупен промет во денари"
+        FROM stock_data
+        WHERE Издавач = ? AND Датум BETWEEN ? AND ?
+        ORDER BY Датум
+    ''', (stock_name, start_date, end_date)).fetchall()
+
+    return data
